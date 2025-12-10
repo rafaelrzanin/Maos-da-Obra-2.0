470f37d3afca5b1d98a173cbafd3c5dc82c5d454
rafaelrzanin <rafaelzanin@gmail.com>
2025-12-10 14:05:32 -0300

    Update WorkDetail.tsx

--- pages/WorkDetail.tsx
+++ pages/WorkDetail.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { dbService } from '../services/db';
-import { Work, Worker, Supplier, Material, Step, StepStatus } from '../types';
+import { Work, Worker, Supplier, Material, Step } from '../types';
 import { ZeModal } from '../components/ZeModal';
 import { FULL_MATERIAL_PACKAGES } from '../services/standards';
 
